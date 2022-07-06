a4a7db159975b60b76fc02ac2423963a60e84f6c
olu <olu@okayhouse.com>
2022-07-05 21:30:20 -0700

    CircleObjects

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace CircleObjects
+{
+    public class Circle
+    {
+        public double radius;
+
+        //Constructor
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+        //Prompts user to enter a radius; user may enter a decimal number.
+        //Calculate circumference and area.
+        //calculate the circumference
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        //calculate the area
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+        }
+        public string CalculateFormattedCircumference()
+        {
+
+            return FormatNumber(CalculateCircumference());
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+
+        }
+    }
+}
+
+//public Circle(double radius)
+//public double CalculateCircumference()
+//public double CalculateArea()
+//private string FormatNumber(double x)
+//public string CalculateFormattedCircumference()
+//public string CalculateFormattedArea()
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,56 @@
+﻿//CircleObjects
+using CircleObjects;
+
+Console.WriteLine("Welcome to Circle Objects!");
+
+int entryCount = 0;
+
+//Program loop.
+bool RunAgain = true;
+while (RunAgain)
+{
+    //Validate data entry.
+    try
+    {
+        //Ask for user input.
+        Console.Write("Please enter a radius: ");
+        double circleRadius = Convert.ToDouble(Console.ReadLine());
+
+        //Call in the methods.
+        Circle myCircle = new Circle(circleRadius);
+
+        //Count number of input per user.
+        entryCount++;
+
+        //Display circumference and area.
+        Console.WriteLine("\nCircumference: " + myCircle.CalculateCircumference());
+        Console.WriteLine("\nArea: " + myCircle.CalculateArea());
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Invalid entry, please try again using a number.");
+
+    }
+
+    //Loop to continue.
+    while (true)
+    {
+        Console.Write("\nWould you like to go again? y/n ");
+        string response = Console.ReadLine().ToLower().Trim();
+        if (response == "y")
+        {
+            break;
+        }
+        else if (response == "n")
+        {
+            Console.WriteLine($"Goodbye. You created {entryCount} Circle object(s)!");
+            RunAgain = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, Please try again");
+        }
+    }
+ 
+}
